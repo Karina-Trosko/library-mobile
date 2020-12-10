f9fe2f0e44c6f7fc2b76ae00c8202fb467c2c323
Karina Trosko <karyna_trasko@epam.com>
2020-12-11 00:37:38 +0300

    init for mac

--- android/app/src/main/java/com/library/MainApplication.java
+++ android/app/src/main/java/com/library/MainApplication.java
@@ -3,10 +3,10 @@ package com.library;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.swmansion.reanimated.ReanimatedPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,10 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReanimatedPackage(),
             new SafeAreaContextPackage(),
             new RNScreensPackage(),
-            new RNGestureHandlerPackage(),
-            new ReanimatedPackage()
+            new RNGestureHandlerPackage()
       );
     }
 
